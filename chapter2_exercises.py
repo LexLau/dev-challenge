7233cb7872d10c97397a46d1fa169b97d8017dfb
Alex Lau <aznalix@yahoo.com>
2014-06-05 20:23:29 -0400

    new origin

--- chapter2_exercises.py
+++ chapter2_exercises.py
@@ -1,3 +1,15 @@
 # Exercises for chapter 2:
 
-# Name:
+# Name:Alex Lau
+
+#2.1
+#zipcode = 02492
+#zipcode returns error
+#zipcode = 02132
+#zipcode returns 1114
+#zipcode = 01, 010, 0100 and 01000 returns 1, 8, 512
+#these values above indicate the binary numbers.
+#calling zipcode would return the numbers these binary nubmers represent.
+
+
+
