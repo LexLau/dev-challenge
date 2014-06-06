3e0a2dd26ff7b45d621133cd200fe3b99092895c
Alex Lau <aznalix@yahoo.com>
2014-06-05 20:40:13 -0400

    first commit

--- chapter2_exercises.py
+++ chapter2_exercises.py
@@ -2,7 +2,7 @@
 
 # Name:Alex Lau
 
-#2.1
+#2.1 done in python interpreter
 #zipcode = 02492
 #zipcode returns error
 #zipcode = 02132
@@ -11,5 +11,17 @@
 #these values above indicate the binary numbers.
 #calling zipcode would return the numbers these binary nubmers represent.
 
+#2.2 also done in python interpreter
+print 'Exercise 2.2'
 
+5
+x = 5
+x + 1
+
+print 'there is no output because there is no print statement'
+
+print '5'
+print 'x = 5'
+print 'x + 1'
+print 'x = ' x
 
